2121a79a4b427b65f9c02eb22feda1fc2e4ae823
achebotar <a.chebotar@um-agency.com>
2019-04-25 16:58:30 +0300

    support androidX

--- vksdk_library/src/main/java/com/vk/sdk/VKServiceActivity.java
+++ vksdk_library/src/main/java/com/vk/sdk/VKServiceActivity.java
@@ -27,11 +27,10 @@ import android.app.Fragment;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import android.text.TextUtils;
 
 import com.vk.sdk.api.VKError;
--- vksdk_library/src/main/java/com/vk/sdk/api/VKSyncRequestUtil.java
+++ vksdk_library/src/main/java/com/vk/sdk/api/VKSyncRequestUtil.java
@@ -22,11 +22,7 @@
 
 package com.vk.sdk.api;
 
-import android.support.annotation.NonNull;
-
-import com.vk.sdk.api.VKError;
-import com.vk.sdk.api.VKRequest;
-import com.vk.sdk.api.VKResponse;
+import androidx.annotation.NonNull;
 
 class VKSyncRequestUtil {
 
